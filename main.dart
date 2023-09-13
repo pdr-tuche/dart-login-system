c1bc8c731ba280798e64c412b1e590358bd7d440
Pedro Neves <contato.nevespedro@gmail.com>
2023-09-13 13:54:22 -0300

    colocado algumas funcoes

--- main.dart
+++ main.dart
@@ -2,26 +2,40 @@ import 'lib/user.dart';
 import 'dart:io';
 
 var users = [];
-void main(List<String> args) {
+int main(List<String> args) {
   String voltar;
   do{
-    print('[1] para adicionar um novo registro\n[2] para mostrar os registros\n[0] para sair');
-    int opcao = int.parse(stdin.readLineSync()!);
+    int opcao = menu();
     switch(opcao){
       case 1:
-        print('digite seu username: ');
-        String username = stdin.readLineSync()!;
-        print('digite a senha: ');
-        String password = stdin.readLineSync()!;
-
-        User user = User(username, password, gerarId(users));
-        users.add(user);
+        criarUser();
       case 2:
         users.isEmpty == true ? print('lista vazia') : print(users);
       default:
-        break;
+        return 0;
     }
     print('deseja voltar para o menu?[S][N]');
     voltar = stdin.readLineSync()!.toUpperCase();
+    if(Platform.isWindows)
+      print(Process.runSync("cls", [], runInShell: true).stdout);
+    else
+      print(Process.runSync('clear', [],runInShell: true).stdout);
+    
   }while(voltar != 'N');
+  return 0;
+}
+
+int menu() {
+  print('[1] para adicionar um novo registro\n[2] para mostrar os registros\n[0] para sair');
+  return int.parse(stdin.readLineSync()!);
+}
+
+
+void criarUser(){
+  print('digite seu username: ');
+  String username = stdin.readLineSync()!;
+  print('digite a senha: ');
+  String password = stdin.readLineSync()!;
+  User user = User(username, password, gerarId(users));
+  users.add(user);
 }
